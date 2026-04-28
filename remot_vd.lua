5c81e837595a5acf5a18ac33e9abb1e9a625654d
Z User <z@container>
2026-04-28 18:18:01 +0000

    Add: remot_vd.lua - file untuk daftar remote VD

---
+++ remot_vd.lua
@@ -0,0 +1,6 @@
+-- ============================================================
+-- REMOTE VD — Daftar Remote Violence District
+-- Paste remote yang kamu dapat dari RemoteSpy di sini
+-- Jangan ada remote yang double!
+-- ============================================================
+
